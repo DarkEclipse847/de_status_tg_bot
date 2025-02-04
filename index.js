9c996c309a3ce686ca95159dd9a6329749beeadc
DarkEclipse847 <ilyapetrov847@gmail.com>
2025-02-04 06:36:15 +0300

    created simple logic to return chat_id

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 require("dotenv").config();
 const {Telegraf} = require("telegraf");
+const Markup = require("telegraf/markup")
 
 const bot = new Telegraf(process.env.TOKEN);
 
-bot.start((ctx)=>ctx.reply("Hello world"));
+//TODO: Need to make this unique, to find requests by id
+var request_chat = 1;
+
+//Creating button on start
+bot.start((ctx)=>{
+    return ctx.reply("Hi", {
+        ...Markup.keyboard([
+            [Markup.button.channelRequest("Выбрать канал", request_chat)]
+        ]).resize()
+    });
+});
+
+//This function will trigger, when user shares chat with bot
+//for now it sends chat_id back to user
+bot.on("chat_shared", ctx=>{
+    ctx.reply(ctx.update.message.chat_shared.chat_id);
+})
 bot.launch();
